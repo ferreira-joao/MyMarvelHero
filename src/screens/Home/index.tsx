35b8da0c6eb3d7d4eee4b9723c030d1bbda4f08f
Nome Sobrenome <joaovfsc231@gmail.com>
2022-04-23 15:18:48 -0300

    added axios and created first api call

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Container } from "./styles";
 
 import { MainHeader } from "../../components/MainHeader";
 
+import { getCharacters } from "../../services/apiCalls";
+
 export function Home() {
+  const [characters, setCharacters] = useState([]);
+
+  const handleCharacters = async () => {
+    const list = await getCharacters();
+
+    setCharacters(list);
+  };
+
+  useEffect(() => {
+    console.log(characters);
+  }, []);
+
   return (
     <Container>
       <MainHeader />
---
+++ src/services/apiCalls.ts
@@ -0,0 +1,13 @@
+import api from "./api";
+
+const apikey = "64757ea405a69c140356a9bb10b0e798";
+const hash = "a22daf704e488df06b9a591e5c1f6c9a";
+
+export function getCharacters() {
+  return api
+    .get(`/characters?ts=1&apikey=${apikey}&hash=${hash}`)
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => console.log("Erro: " + err));
+}
